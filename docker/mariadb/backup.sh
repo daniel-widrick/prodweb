39cd18f14032eba3308f7c127cf5fbebeb432fd8
Daniel Widrick <daniel@widrick.net>
2024-11-23 00:24:51 -0500

    add backup age off for database

--- docker/mariadb/backup.sh
+++ docker/mariadb/backup.sh
@@ -12,3 +12,6 @@ backup_date=$(date +%Y%m%d_%H%M%S)
 docker exec $dbcontainer mariadb-dump -p${MYSQL_ROOT_PASSWORD} --all-databases | gzip > /opt/docker-data/db-backups/db_${backup_date}.sql.gz
 
 cp /opt/docker-data/db-backups/db_${backup_date}.sql.gz /opt/docker/db.sql.gz
+
+#Clean up
+find /opt/docker-data/db-backups/ -type f -mtime +365 -exec rm -f {} +
